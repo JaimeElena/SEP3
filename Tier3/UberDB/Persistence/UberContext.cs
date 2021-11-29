c5eaa9ee11a8f75f13277305196512f01dfd29d5
Siyu <59789515+TROY571@users.noreply.github.com>
2021-11-29 13:59:57 +0100

    Add files via upload

---
+++ Tier3/UberDB/Persistence/UberContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UberDB.Models;
+
+namespace UberDB.Persistence
+{
+    public class UberContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // name of database
+            optionsBuilder.UseSqlite(@"Data Source = D:\UberDB\UberDB\UberDB.db");
+        }
+    }
+}
