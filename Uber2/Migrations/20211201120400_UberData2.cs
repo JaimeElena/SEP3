7a67c9134b94c2fba42c88a86ba2392b72c9b093
Siyu <1021109492@qq.com>
2021-12-01 13:55:01 +0100

    ...

---
+++ Uber2/Migrations/20211201120400_UberData2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uber2.Migrations
+{
+    public partial class UberData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isLogged",
+                table: "Drivers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isLogged",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isLogged",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "isLogged",
+                table: "Customers");
+        }
+    }
+}
