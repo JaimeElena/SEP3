bfcb8e55943fe9fb04a3cdb3ff6474229e828853
Siyu <1021109492@qq.com>
2021-12-08 14:31:24 +0100

    add more migrations

---
+++ Uber2/Migrations/20211207160745_UberData5.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uber2.Migrations
+{
+    public partial class UberData5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isAccepted",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "isCompleted",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isAccepted",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isCompleted",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
