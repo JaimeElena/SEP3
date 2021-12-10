8f68cd2dbee582ebab291a95f0d1fe86d6030ec8
Lokkaze <74102422+Lokkaze@users.noreply.github.com>
2021-12-10 18:05:10 +0100

    Fix bug

--- UberT1Costumer/Models/Order.cs
+++ UberT1Costumer/Models/Order.cs
@@ -6,6 +6,7 @@
         public string status { get; set; }
         public string estimatetime { get; set; }
         public Driver driver { get; set; }
+        public Costumer costumer { get; set; }
         public Location customerlocation { get; set; }
         public Location destination { get; set; }
         public double price { get; set; }
