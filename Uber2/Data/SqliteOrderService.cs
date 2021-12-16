b98725579a3152b9d081f3bb4178e65c436ee177
Siyu <1021109492@qq.com>
2021-12-16 18:34:17 +0100

    fix GetHistoryOrders

--- Uber2/Data/SqliteOrderService.cs
+++ Uber2/Data/SqliteOrderService.cs
@@ -66,7 +66,7 @@ namespace Uber2.Data
                 IList<Order> list = new List<Order>();
                 foreach (var order in all)
                 {
-                    if (order.status == "Completed"||order.status == "Cancelled" && order.customer == customer)
+                    if ((order.status.Equals("Completed")||order.status.Equals("Cancelled")) && order.customer.Equals(customer))
                     {
                         list.Add(order);
                     }
